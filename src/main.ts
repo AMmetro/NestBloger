0efc39d525f90c73a84adc1f081b69100dae8c30
AMmetro <metro.trend.now@gmail.com>
2024-04-03 10:02:45 +0300

    back branch

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { appConfig } from './settings/appConfig';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(appConfig.PORT || 5000, () =>
-    console.log(`app started on port ${appConfig.PORT}`),
-  );
+  await app.listen(3000);
 }
 bootstrap();
